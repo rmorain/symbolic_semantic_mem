7dc1f1edc958a2e20c44deb90f7578db85035e76
Rob Morain <rob.morain@mx.com>
2021-01-09 17:07:32 -0700

    Basic model and run params added

--- kirby/experiment.py
+++ kirby/experiment.py
@@ -4,6 +4,19 @@ __all__ = ['Experiment']
 
 # Cell
 class Experiment():
-    def run(self, run_params):
-        "Given a set of run parameters, run an experiment"
-        pass
+    def __init__(self, run_params):
+        self.model = model(run_params.model)
+        self.run_params = run_params
+
+    def run(self):
+        trainer = pl.Trainer(
+            default_root_dir='logs',
+            gpus=(1 if torch.cuda.is_available() else 0),
+            max_epochs=self.run_params.max_epochs,
+            fast_dev_run=self.run_params.debug,
+            logger=WandbLogger(save_dir='logs/',
+                                name=self.run_params.run_name,
+                                project=self.run_params.project_name),
+        )
+
+        trainer.fit(model)
