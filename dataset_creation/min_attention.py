862f8efc22fdd9b9c393cf44eca2540f8ba74735
Robert Morain <robert.morain@gmail.com>
2021-08-12 12:41:25 -0600

    Min attention dataset

---
+++ dataset_creation/min_attention.py
@@ -0,0 +1,34 @@
+import json
+
+import pandas as pd
+from tqdm import tqdm
+
+
+def get_knowledge(entity_string, knowledge_list):
+    for k in knowledge_list:
+        if k and k["label"] == entity_string:
+            return k["description"]
+    return None
+
+
+df = pd.read_pickle("data/augmented_datasets/pickle/sorted_attentions_valid.pkl")
+debug = False
+with tqdm(total=df.shape[0]) as pbar:
+    for index, row in df.iterrows():
+        # Reverse entities to get min
+        row["entities"].sort(reverse=False, key=lambda x: x[1])
+        for entity in row["entities"]:
+            entity = entity[0]
+            description = get_knowledge(entity, row["knowledge"])
+            if description:
+                k_dict = {"label": entity, "description": description}
+                # Save as JSON string
+                row["knowledge"] = json.dumps(k_dict)
+                break
+        if description is None:
+            row["knowledge"] = ""
+        pbar.update(1)
+df = df.drop("entities", axis=1)
+if not debug:
+    df.to_pickle("data/augmented_datasets/pickle/min_attention_valid.pkl")
+print("Finished")
---
+++ experiments/03_min_attention.py
@@ -0,0 +1,20 @@
+from kirby.experiment import Experiment
+from kirby.knowledge_model import KnowledgeModel
+from kirby.run_params import RunParams
+
+data_files = {
+    "train": ["data/augmented_datasets/pickle/min_attention.pkl"],
+    "valid": ["data/augmented_datasets/pickle/min_attention_valid.pkl"],
+}
+run_params = RunParams(
+    run_name="max_attention",
+    debug=True,
+    pretrained=True,
+    data_files=data_files,
+    data_file_type="pandas",
+    knowledge_tokenize=True,
+)
+model = KnowledgeModel(run_params)
+
+experiment = Experiment(run_params, model)
+experiment.run()
