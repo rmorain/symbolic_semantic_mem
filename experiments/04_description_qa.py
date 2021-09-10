113c359a12f969776fcd1de40266ffe6b06f69ab
Robert Morain <robert.morain@gmail.com>
2021-09-10 14:36:24 -0600

    Batching added

--- experiments/04_description_qa.py
+++ experiments/04_description_qa.py
@@ -7,13 +7,13 @@ data_files = {
     "valid": "data/augmented_datasets/pickle/description_qa_val.pkl",
 }
 run_params = RunParams(
-    run_name="description_qa",
+    run_name="description_qa_test",
     debug=True,
     pretrained=True,
     data_files=data_files,
     data_file_type="pandas",
     knowledge_tokenize=False,
-    batch_size=1,
+    batch_size=8,
 )
 model = QAModel(run_params)
 
--- kirby/qa_model.py
+++ kirby/qa_model.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import torch
+import torch.nn.functional as F
+import torchmetrics
 from datasets import Dataset
 from pytorch_lightning import LightningModule
 from transformers import GPT2Config, GPT2DoubleHeadsModel, GPT2Tokenizer
@@ -13,8 +15,7 @@ class QAModel(BasicModel, LightningModule):
     def __init__(self, run_params):
         super().__init__(run_params)
         self.run_params = run_params
-        config = GPT2Config()
-        self.model = GPT2DoubleHeadsModel(config)
+        self.model = GPT2DoubleHeadsModel.from_pretrained(self.run_params.model)
         self.loss = torch.nn.CrossEntropyLoss(reduction="none")
         self.tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
         self.tokenizer.pad_token = self.tokenizer.eos_token
@@ -23,6 +24,9 @@ class QAModel(BasicModel, LightningModule):
         self.tokenizer.add_special_tokens({"cls_token": "[CLS]"})
         # Update the model embeddings with the new vocabulary size
         self.model.resize_token_embeddings(len(self.tokenizer))
+
+        self.train_acc = torchmetrics.Accuracy()
+        self.val_acc = torchmetrics.Accuracy()
 
     def prepare_data(self):
         self.setup()
@@ -45,25 +49,25 @@ class QAModel(BasicModel, LightningModule):
         return ds
 
     def tokenize(self, x):
-        question = x["question"]
+        question = x["question"] + " "
         answer = x["correct"] + " [CLS]"
         distractors = [d + " [CLS]" for d in x["distractors"]]
         question_tokens = self.tokenizer(
             question,
-            truncation=True,
         )
         answer_tokens = self.tokenizer(
             answer,
             truncation=True,
             padding="max_length",
-            max_length=self.run_params.seq_length,
+            max_length=self.run_params.seq_length - len(question_tokens["input_ids"]),
         )
         distractor_tokens = [
             self.tokenizer(
                 d,
                 truncation=True,
                 padding="max_length",
-                max_length=self.run_params.seq_length,
+                max_length=self.run_params.seq_length
+                - len(question_tokens["input_ids"]),
             )
             for d in distractors
         ]
@@ -83,37 +87,90 @@ class QAModel(BasicModel, LightningModule):
         result["distractor_token_mask"] = [
             d["attention_mask"] for d in distractor_tokens
         ]
+        input_ids = [
+            result["question_tokens_ids"] + d for d in result["distractor_token_ids"]
+        ]
+        attention_mask = [
+            result["question_tokens_mask"] + d for d in result["distractor_token_mask"]
+        ]
+        input_ids.append(result["question_tokens_ids"] + result["answer_tokens_ids"])
+        attention_mask.append(
+            result["question_tokens_mask"] + result["answer_tokens_mask"]
+        )
+        input_ids = torch.tensor(input_ids).to(self.model.device).long()
+        mc_token_ids = (input_ids == 50257).nonzero(as_tuple=True)[1]
+        input_ids = input_ids.tolist()
+        mc_token_ids = mc_token_ids.tolist()
+        labels = 3
+        result = {
+            "input_ids": input_ids,
+            "attention_mask": attention_mask,
+            "mc_token_ids": mc_token_ids,
+            "labels": labels,
+        }
 
         return result
 
     def forward(self, x):
-        input_ids = [x["question_tokens_ids"] + d for d in x["distractor_token_ids"]]
-        attention_mask = [
-            x["question_tokens_mask"] + d for d in x["distractor_token_mask"]
+        input_ids = self.fix_batch(x["input_ids"])
+        attention_mask = self.fix_batch(x["attention_mask"])
+        mc_token_ids = [
+            [sequence.tolist() for sequence in choice] for choice in x["mc_token_ids"]
         ]
-        input_ids.append(x["question_tokens_ids"] + x["answer_tokens_ids"])
-        attention_mask.append(x["question_tokens_mask"] + x["answer_tokens_mask"])
-        input_ids = torch.tensor(input_ids).to(self.model.device).long()
-        attention_mask = torch.tensor(attention_mask).to(self.model.device).long()
-        attention_mask = torch.reshape(attention_mask, input_ids.shape)
-        mc_token_ids = (input_ids == 50257).nonzero(as_tuple=True)[1]
-        labels = torch.tensor([0, 0, 0, 1]).to(self.model.device).long()
-        # Shuffle everything
-        random_indices = torch.randperm(input_ids.shape[0])
-        input_ids = input_ids[random_indices]
-        attention_mask = attention_mask[random_indices]
-        mc_token_ids = mc_token_ids[random_indices]
-        labels = labels[random_indices]
-        labels = torch.argmax(labels)
-        # Unsqueeze
-        input_ids.unsqueeze_(0)
-        attention_mask.unsqueeze_(0)
-        mc_token_ids.unsqueeze_(0)
+        mc_token_ids = torch.tensor(mc_token_ids, device=self.model.device)
+        mc_token_ids = mc_token_ids.transpose(0, 1)
+        mc_token_ids = mc_token_ids.reshape((self.run_params.batch_size, 4))
+        mc_token_ids = mc_token_ids.contiguous()
+        labels = x["labels"]
         # Forward
-        loss = self.model(
+        outputs = self.model(
             input_ids,
             attention_mask=attention_mask,
             mc_token_ids=mc_token_ids,
             mc_labels=labels,
-        ).mc_loss
+        )
+        preds = F.softmax(outputs.mc_logits, dim=1)
+
+        loss = outputs.mc_loss
+        targets = labels.clone().detach()
+        return loss, preds, targets
+
+    def fix_batch(self, choices):
+        result = [
+            [[t.tolist() for t in sequence] for sequence in choice]
+            for choice in choices
+        ]
+        result = torch.tensor(result, device=self.model.device)
+        result = result.transpose(2, 1)
+        result = result.transpose(1, 0)
+        result = result.contiguous()
+        return result
+
+    def training_step(self, batch, batch_idx):
+        loss, preds, targets = self.forward(batch)
+        self.log("train_loss", loss)
+        return loss, preds, targets
+
+    def training_step_end(self, batch):
+        loss, preds, targets = batch
+        self.train_acc(preds, targets)
         return loss
+
+    def validation_step(self, batch, batch_idx):
+        loss, preds, targets = self.forward(batch)
+
+        self.log("val_loss", loss)
+        return loss, preds, targets
+
+    def validation_step_end(self, batch):
+        loss, preds, targets = batch
+        self.val_acc(preds, targets)
+        return loss
+
+    def training_epoch_end(self, losses):
+        self.log("train_acc_epoch", self.train_acc.compute())
+
+    def validation_epoch_end(self, losses):
+        loss = torch.cat([loss.unsqueeze(0) for loss in losses], 0).mean()
+        self.log("val_loss", loss)
+        self.log("val_acc_epoch", self.val_acc.compute())
